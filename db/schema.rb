05f830f3b1646e3f5051df2bc23f1213f64625a7
Neil Gehani <njgonline@me.com>
2013-10-01 22:25:30 -0700

    Fixed Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20130828044830) do
     t.string   "remember_token"
   end
 
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
