38c83b088eb8557321b4329bf708de4fdcd16120
Neil Gehani <njgonline@me.com>
2013-10-01 22:29:59 -0700

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,7 @@ SampleApp::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # Listing 7.29
   config.force_ssl = true
 
   # Set to :debug to see everything in the log.
