620926c66a07470f3ff458d0ab536ccbbd5cfcae
Neil Gehani <njgonline@me.com>
2013-10-02 18:47:55 -0700

    Add deleted Migration

--- db/migrate/20130828044830_add_remember_token_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddRememberTokenToUsers < ActiveRecord::Migration
-  def change
-  	# Created by rails generate migration add_remember_token_to_users
-  	# This adds and indexes remember_token to the users table
-  	add_column :users, :remember_token, :string
-    add_index  :users, :remember_token
-  end
-end
